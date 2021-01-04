7f3425c37b140d37404826f2c6e13244e3d1da6c
Hans Nacker <nackerhans@gmx.de>
2021-01-04 17:50:14 +0100

    Add clicki clicki

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components/macro";
 
 export default function Cards({ imgSrc }) {
+  const [clicked, setClicked] = useState(false);
+
   return (
-    <CardContainer>
+    <CardContainer activated={clicked} onClick={() => setClicked(!clicked)}>
+      <div activated={clicked} />
       <img src={imgSrc} alt="Bild" />
     </CardContainer>
   );
@@ -13,6 +16,17 @@ const CardContainer = styled.div`
   width: 180px;
   height: 180px;
   background-color: black;
+  position: relative;
+  div {
+    background-color: ${(props) => (props.activated ? "transparent" : "green")};
+    /*background-color: ${(props) => console.log(props.activated)};*/
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+  }
   img {
     width: 100%;
     height: 100%;
