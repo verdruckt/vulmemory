2df4f78b46add5f1a70dadbf9e227fdecbd71bf7
Hans Nacker <nackerhans@gmx.de>
2021-01-05 01:30:12 +0100

    change pics

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components/macro";
 import pic1 from "../assets/1.jpg";
-import pic2 from "../assets/2.png";
+import pic2 from "../assets/2.jpg";
 import pic3 from "../assets/3.jpg";
 import pic4 from "../assets/4.jpg";
 import pic5 from "../assets/5.jpg";
